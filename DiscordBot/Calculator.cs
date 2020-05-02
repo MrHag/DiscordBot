5574ccfc86af58bcdbb6481e2ee5d46a5492e0d0
superhackervit <superhackervit@gmail.com>
2020-05-02 16:55:25 +0300

    Edited

---
+++ DiscordBot/Calculator.cs
@@ -0,0 +1,316 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CSharpCALC
+{
+    internal static class Calculator
+    {
+        private static Calculator.number cal(List<Calculator.number> g, int index)
+        {
+            Calculator.number res = new Calculator.number();
+            res.type = Calculator.number.types.Int;
+            if (index - 1 < 0 || index + 1 >= g.Count)
+            {
+                throw new Exception("![]");
+            }
+            if (g[index - 1].type == Calculator.number.types.String)
+            {
+                g[index - 1] = Calculator.calculate(g[index - 1].val);
+                if (g[index - 1].type == Calculator.number.types.Error)
+                {
+                    return g[index - 1];
+                }
+            }
+            if (g[index + 1].type == Calculator.number.types.String)
+            {
+                g[index + 1] = Calculator.calculate(g[index + 1].val);
+                if (g[index + 1].type == Calculator.number.types.Error)
+                {
+                    return g[index + 1];
+                }
+            }
+            double num = double.Parse(g[index - 1].val);
+            double num2 = double.Parse(g[index + 1].val);
+            char c = g[index].val[0];
+            switch (c)
+            {
+                case '*':
+                    res.val = (num * num2).ToString();
+                    break;
+                case '+':
+                    res.val = (num + num2).ToString();
+                    break;
+                case ',':
+                case '.':
+                    break;
+                case '-':
+                    res.val = (num - num2).ToString();
+                    break;
+                case '/':
+                    if (num2 == 0.0)
+                    {
+                        throw new Exception("/0");
+                    }
+                    res.val = (num / num2).ToString();
+                    break;
+                default:
+                    if (c == '^')
+                    {
+                        res.val = Math.Pow(num, num2).ToString();
+                    }
+                    break;
+            }
+            return res;
+        }
+
+        public static Calculator.number calculate(string data)
+        {
+            Calculator.number result;
+            try
+            {
+                data = data.Replace(" ", "").Replace('.', ',');
+                int last_num = -1;
+                bool open = false;
+                int opens_count = 0;
+                bool num = false;
+                bool default_num = true;
+                List<Calculator.number> arr = new List<Calculator.number>();
+                for (int i = 0; i < data.Length; i++)
+                {
+                    if (data[i] == '+' || data[i] == '-' || data[i] == '*' || data[i] == '/' || data[i] == '^')
+                    {
+                        if (num && !open)
+                        {
+                            num = false;
+                            int last_num_lenght = i - last_num;
+                            Calculator.number.types tmp;
+                            if (default_num)
+                            {
+                                tmp = Calculator.number.types.Int;
+                            }
+                            else
+                            {
+                                tmp = Calculator.number.types.String;
+                            }
+                            arr.Add(new Calculator.number(data.Substring(last_num, last_num_lenght), tmp));
+                            default_num = true;
+                            arr.Add(new Calculator.number(data[i].ToString(), Calculator.number.types.Do));
+                        }
+                        if (open)
+                        {
+                            default_num = false;
+                        }
+                        if (!num)
+                        {
+                            if (i > 0)
+                            {
+                                if (data[i - 1] == '+' || data[i - 1] == '-' || data[i - 1] == '*' || data[i - 1] == '/' || data[i - 1] == '^')
+                                {
+                                    throw new Exception("+-");
+                                }
+                            }
+                            else
+                            {
+                                if (data[i] != '-')
+                                {
+                                    throw new Exception("+-");
+                                }
+                                num = true;
+                                last_num = i;
+                            }
+                        }
+                    }
+                    else
+                    {
+                        if (data[i] == '(')
+                        {
+                            opens_count++;
+                        }
+                        if (data[i] == ')')
+                        {
+                            opens_count--;
+                        }
+                        if (opens_count < 0)
+                        {
+                            throw new Exception("!()");
+                        }
+                        if (!num)
+                        {
+                            if (data[i] == '(')
+                            {
+                                open = true;
+                                num = true;
+                                last_num = i + 1;
+                            }
+                            else if (int.Parse(data[i].ToString()) != -1)
+                            {
+                                num = true;
+                                last_num = i;
+                                if (i == data.Length - 1)
+                                {
+                                    num = false;
+                                    int last_num_lenght = i + 1 - last_num;
+                                    Calculator.number.types tmp2;
+                                    if (default_num)
+                                    {
+                                        tmp2 = Calculator.number.types.Int;
+                                    }
+                                    else
+                                    {
+                                        tmp2 = Calculator.number.types.String;
+                                    }
+                                    arr.Add(new Calculator.number(data.Substring(last_num, last_num_lenght), tmp2));
+                                    default_num = true;
+                                }
+                            }
+                        }
+                        else if (open)
+                        {
+                            if (data[i] == ')' && opens_count == 0)
+                            {
+                                open = false;
+                                num = false;
+                                int last_num_lenght = i - last_num;
+                                Calculator.number.types tmp3;
+                                if (default_num)
+                                {
+                                    tmp3 = Calculator.number.types.Int;
+                                }
+                                else
+                                {
+                                    tmp3 = Calculator.number.types.String;
+                                }
+                                arr.Add(new Calculator.number(data.Substring(last_num, last_num_lenght), tmp3));
+                                if (i != data.Length - 1)
+                                {
+                                    arr.Add(new Calculator.number(data[i + 1].ToString(), Calculator.number.types.Do));
+                                }
+                                default_num = true;
+                            }
+                        }
+                        else if (i == data.Length - 1)
+                        {
+                            num = false;
+                            int last_num_lenght = i + 1 - last_num;
+                            Calculator.number.types tmp4;
+                            if (default_num)
+                            {
+                                tmp4 = Calculator.number.types.Int;
+                            }
+                            else
+                            {
+                                tmp4 = Calculator.number.types.String;
+                            }
+                            arr.Add(new Calculator.number(data.Substring(last_num, last_num_lenght), tmp4));
+                            default_num = true;
+                        }
+                    }
+                }
+                Calculator.number res = new Calculator.number();
+                string do_arr = new string("*/+-^".ToCharArray());
+                if (arr.Count == 1)
+                {
+                    if (arr[0].type == Calculator.number.types.String)
+                    {
+                        arr.Add(new Calculator.number("*", Calculator.number.types.Do));
+                        arr.Add(new Calculator.number("1", Calculator.number.types.Int));
+                    }
+                    else
+                    {
+                        if (arr[0].val != "")
+                        {
+                            return arr[0];
+                        }
+                        throw new Exception("undefined");
+                    }
+                }
+                for (int j = 0; j < do_arr.Length; j += 2)
+                {
+                    for (int k = 0; k < arr.Count; k++)
+                    {
+                        if (arr[k].type == Calculator.number.types.Do)
+                        {
+                            res.type = Calculator.number.types.Int;
+                            if (arr[k].val == do_arr[j].ToString() || arr[k].val == do_arr[j + 1].ToString())
+                            {
+                                arr[k - 1] = Calculator.cal(arr, k);
+                                if (arr[k - 1].type == Calculator.number.types.Error)
+                                {
+                                    return arr[k - 1];
+                                }
+                                arr.RemoveAt(k);
+                                arr.RemoveAt(k);
+                                k--;
+                            }
+                            res.val = arr[k].val;
+                        }
+                    }
+                }
+                result = res;
+            }
+            catch (Exception error)
+            {
+                Calculator.number re = new Calculator.number();
+                re.type = Calculator.number.types.Error;
+                if (error.Message == "/0")
+                {
+                    re.val = "Деление 0";
+                    result = re;
+                }
+                else if (error.Message == "!()")
+                {
+                    re.val = "Не коректно раставлены скобки ()";
+                    result = re;
+                }
+                else if (error.Message == "+-")
+                {
+                    re.val = "Не коректно раставлены знаки действий";
+                    result = re;
+                }
+                else if (error.Message == "undefined")
+                {
+                    re.val = "Не возможно определить число";
+                    result = re;
+                }
+                else if (error.Message == "![]")
+                {
+                    re.val = "Не коректно раставлены елементов примера";
+                    result = re;
+                }
+                else
+                {
+                    re.val = error.Message;
+                    result = re;
+                }
+            }
+            return result;
+        }
+
+        public class number
+        {
+            public string val { get; set; }
+            public Calculator.number.types type { get; set; }
+
+            public number(string v, Calculator.number.types t)
+            {
+                this.val = v;
+                this.type = t;
+            }
+
+            public number()
+            {
+                this.val = "";
+                this.type = Calculator.number.types.NULL;
+            }
+
+            public enum types
+            {
+                NULL,
+                Int,
+                String,
+                Do,
+                Error
+            }
+        }
+    }
+}
---
+++ DiscordBot/cm.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Security;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+using CSharpCALC;
+using Discord;
+using Discord.Commands;
+using HtmlAgilityPack;
+
+namespace D2
+{
+    public class cm : ModuleBase<SocketCommandContext>
+    {
+        [Command("say")]
+        [Summary("Echoes a message.")]
+        public Task SayAsync([Remainder] [Summary("The text to echo")] string echo)
+        {
+            return this.ReplyAsync(echo, false, null, null);
+        }
+
+        [Command("Calc", RunMode = RunMode.Async)]
+        public async Task CalcAsync([Remainder] string exc = null)
+        {
+            if (exc != null)
+            {
+                await this.ReplyAsync(exc + " = " + Calculator.calculate(exc).val.Replace(',', '.'), false, null, null);
+            }
+        }
+
+        [Command("$", RunMode = RunMode.Async)]
+        public async Task DolAsync([Remainder] string num = "1")
+        {     
+            try
+            {
+                
+                HtmlNode tbody = await ParseSite();
+                double init = double.Parse(num.Replace('.', ','));
+                await Context.Channel.SendMessageAsync(DolAsync(tbody, init, "UAH")
+                                                +'\n'+ DolAsync(tbody, init, "RUB")
+                                                +'\n'+ DolAsync(tbody, init, "BYN")
+                                                +'\n'+ DolAsync(tbody, init, "CNY")
+                                                +'\n'+ DolAsync(tbody, init, "EUR"),
+                                                false, null, null);
+                return;
+            }
+            catch (Exception ex)
+            {
+                await Program.LogAsync(new LogMessage(LogSeverity.Error, nameof(DolAsync), ex.Message, ex));
+            }
+            
+        }
+
+        public async Task<HtmlNode> ParseSite()
+        {
+
+                Uri Url = new Uri("https://bank.gov.ua/markets/exchangerates");
+                string content = await HTTPDownload(Url);
+                HtmlDocument html = new HtmlDocument();
+                html.LoadHtml(content);
+                HtmlNode tbody = html.GetElementbyId("exchangeRates").ChildNodes.ElementAt(3);
+                return tbody;
+
+        }
+
+        [Command("help", RunMode = RunMode.Async)]
+        public async Task HAsync([Remainder] string find = null)
+        {
+            if ((base.Context.Channel as ITextChannel).IsNsfw)
+            {
+                Uri Url;
+                if (find != null)
+                {
+                    Url = new Uri("https://rt.pornhub.com/video/search?search=" + find);
+                }
+                else
+                {
+                    Url = new Uri("https://rt.pornhub.com");
+                }
+                string content = await this.HTTPDownload(Url);
+                HtmlDocument htmlDocument = new HtmlDocument();
+                htmlDocument.LoadHtml(content);
+                List<string> links = new List<string>();
+                foreach (HtmlNode a in htmlDocument.DocumentNode.SelectNodes("//a"))
+                {
+                    if (a.HasClass("linkVideoThumb"))
+                    {
+                        links.Add("http://rt.pornhub.com" + a.GetAttributeValue("href", ""));
+                    }
+                }
+                for (int i = 0; i < ((5 > links.Count<string>()) ? links.Count<string>() : 5); i++)
+                {
+                    await Context.Channel.SendMessageAsync(links[Program.Rand.Next(0, links.Count<string>() - 1)], false, null, null);
+                }
+                links = null;
+            }
+        }
+
+        public async Task<string> HTTPDownload(Uri ResourceURI)
+        {
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+            ServicePointManager.ServerCertificateValidationCallback = (RemoteCertificateValidationCallback)Delegate.Combine(ServicePointManager.ServerCertificateValidationCallback, new RemoteCertificateValidationCallback((object s, X509Certificate cert, X509Chain ch, SslPolicyErrors sec) => true));
+            ServicePointManager.DefaultConnectionLimit = 50;
+            CookieContainer cookieContainer = new CookieContainer();
+            HttpWebRequest httpRequest = WebRequest.CreateHttp(ResourceURI);
+            try
+            {
+                httpRequest.CookieContainer = cookieContainer;
+                httpRequest.Timeout = (int)TimeSpan.FromSeconds(15.0).TotalMilliseconds;
+                httpRequest.AllowAutoRedirect = true;
+                httpRequest.AutomaticDecompression = (DecompressionMethods.GZip | DecompressionMethods.Deflate);
+                httpRequest.ServicePoint.Expect100Continue = false;
+                httpRequest.UserAgent = "Mozilla / 5.0(Windows NT 6.1; WOW32; Trident / 7.0; rv: 11.0) like Gecko";
+                httpRequest.Accept = "ext/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8";
+                httpRequest.Headers.Add(HttpRequestHeader.AcceptEncoding, "gzip, deflate;q=0.8");
+                httpRequest.Headers.Add(HttpRequestHeader.CacheControl, "no-cache");
+
+                return new StreamReader(
+                    (await httpRequest.GetResponseAsync()).GetResponseStream()
+                                       ).ReadToEnd();
+                
+            }
+            
+            catch (Exception ex)
+            {
+
+                await Program.LogAsync(new LogMessage(LogSeverity.Error, nameof(HTTPDownload)+": ", ex.Message, ex));
+
+            }
+            return "";
+        }
+
+        internal static double DolAsync(HtmlNode tbody, string type)
+		{
+			for (int i = 1; i < tbody.ChildNodes.Count; i += 2)
+			{
+				HtmlNode tr = tbody.ChildNodes.ElementAt(i);
+				if (tr.ChildNodes.ElementAt(3).InnerHtml == type)
+				{
+					return (float.Parse(tr.ChildNodes.ElementAt(9).InnerHtml) / int.Parse(tr.ChildNodes.ElementAt(5).InnerHtml));
+				}
+}
+			return 0;
+		}
+
+        internal static string DolAsync(HtmlNode tbod, double ini, string type)
+		{
+			string str = ini + " $ = ";
+            double dolini = DolAsync(tbod, "USD") * ini;
+			if (type == "UAH")
+			{
+				str += Math.Round(dolini, 2);
+			}
+			else
+			{
+				str += Math.Round(dolini / DolAsync(tbod, type), 2);
+			}
+			return str + " " + type;
+		}
+	}
+}
